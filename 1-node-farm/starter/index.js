7fb05ddb34011ca4f89363c2e4a33cce065a34e0
Jose Hernandez <jhernandez27603@gmail.com>
2021-02-09 21:02:51 -0500

    create simple web server with node

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//////////////////////////////////////////////////////
+// FILES
 
 // // Blocking, synchronous way
 // // read file input.txt and display content to console 
@@ -9,20 +14,41 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File has been written!');
 
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) {
-    return console.log('ERROR!!!');
-  }
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+// // Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) {
+//     return console.log('ERROR!!!');
+//   }
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been wrriten!!');
-      });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been wrriten!!');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+//////////////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     });
-  });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000!');
+});
