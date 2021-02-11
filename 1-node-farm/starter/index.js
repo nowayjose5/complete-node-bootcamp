2ef73605d38d21027cf50819f39ce683cddade67
Jose Hernandez <jhernandez27603@gmail.com>
2021-02-10 19:43:54 -0500

    setup prettier, which is something I might do more often

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -9,7 +9,7 @@ const replaceTemplate = require('./modules/replaceTemplate');
 // FILES
 
 // // Blocking, synchronous way
-// // read file input.txt and display content to console 
+// // read file input.txt and display content to console
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
 // // wrote content to a file and saved it as output.txt
@@ -37,9 +37,18 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////////////////////////
 // SERVER
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -53,34 +62,34 @@ const server = http.createServer((req, res) => {
   // Overview page
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
-      'Content-type': 'text/html'
+      'Content-type': 'text/html',
     });
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
-  
-  // Product page
+
+    // Product page
   } else if (pathname === '/product') {
     res.writeHead(200, {
-      'Content-type': 'text/html'
+      'Content-type': 'text/html',
     });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
-  // API
+    // API
   } else if (pathname === '/api') {
     res.writeHead(200, {
-      'Content-type': 'application/json'
+      'Content-type': 'application/json',
     });
     res.end(data);
 
-  // Not found
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
-      'my-own-header': 'hello-world'
+      'my-own-header': 'hello-world',
     });
     res.end('<h1>Page not found!</h1>');
   }
