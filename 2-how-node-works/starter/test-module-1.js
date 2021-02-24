ccbe907c8cfd40cd633be86b3e21ab49d0f850fc
Jose Hernandez <jhernandez27603@gmail.com>
2021-02-23 19:26:12 -0500

    done learning about requiring modules

---
+++ 2-how-node-works/starter/test-module-1.js
@@ -0,0 +1,27 @@
+// class Calculator {
+// 	add(a, b) {
+// 		return a + b;
+// 	}
+
+// 	multiplyu(a, b) {
+// 		return a * b;
+// 	}
+
+// 	divide(a, b) {
+// 		return a / b;
+// 	}
+// }
+
+module.exports = class {
+	add(a, b) {
+		return a + b;
+	}
+
+	multiplyu(a, b) {
+		return a * b;
+	}
+
+	divide(a, b) {
+		return a / b;
+	}
+};
